2faf92eff2b1c6d37d58ac60aae417a17e41eec7
DALJEET SINGH <daljeetsingh@DALJEETs-MacBook-Air.local>
2025-06-20 17:33:30 +0530

    added some components,pages in user-app  and onrampTransaction functionality

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -14,8 +14,8 @@ export const Appbar = ({
     onSignin,
     onSignout
 }: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4">
-        <div className="text-lg flex flex-col justify-center">
+    return <div className="flex justify-between border-b px-4 border-slate-300">
+        <div className="text-lg flex flex-col justify-center text-[#6a51a6]">
             MintSafe
         </div>
         <div className="flex flex-col justify-center pt-2">
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,4 +1,5 @@
 "use client";
+"use client";
 
 import { ReactNode } from "react";
 
@@ -9,7 +10,7 @@ interface ButtonProps {
 
 export const Button = ({ onClick, children }: ButtonProps) => {
   return (
-    <button onClick={onClick} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+    <button onClick={onClick} type="button" className="text-white bg-[#6a51a6] hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
       {children}
     </button>
 
